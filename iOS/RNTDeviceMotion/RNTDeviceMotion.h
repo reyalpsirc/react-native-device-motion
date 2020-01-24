34fe8dcebb870ce35aac93d6360fa0b6dfdaa728
Cristiano Santos <cristianomnsantos@gmail.com>
2020-01-24 12:34:43 +0000

    Update RNTDeviceMotion.h
    - RN 0.61.5

--- iOS/RNTDeviceMotion/RNTDeviceMotion.h
+++ iOS/RNTDeviceMotion/RNTDeviceMotion.h
@@ -7,7 +7,11 @@
 //
 
 #import <Foundation/Foundation.h>
-#import  <React/RCTBridgeModule.h>
+#if __has_include(<React/RCTBridgeModule.h>)
+  #import <React/RCTBridgeModule.h>
+#else
+  #import "RCTBridgeModule.h"
+#endif
 
 @interface RNTDeviceMotion : NSObject <RCTBridgeModule>
 
