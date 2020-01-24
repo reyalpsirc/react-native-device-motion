50d1fd168afa5b3d6b7c7f3b6d06a9ea58a50b6f
Cristiano Santos <cristianomnsantos@gmail.com>
2020-01-24 12:40:17 +0000

    Update RNTDeviceMotion.h
    - Revert

--- iOS/RNTDeviceMotion/RNTDeviceMotion.h
+++ iOS/RNTDeviceMotion/RNTDeviceMotion.h
@@ -7,11 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#if __has_include(<React/RCTBridgeModule.h>)
-  #import <React/RCTBridgeModule.h>
-#else
-  #import "RCTBridgeModule.h"
-#endif
+#import <React/RCTBridgeModule.h>
 
 @interface RNTDeviceMotion : NSObject <RCTBridgeModule>
 
